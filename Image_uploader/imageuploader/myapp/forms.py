8d2aeaec648a2db2f98c8d0d6544249c76a2e112
Akriti26 <114924216+Akriti26@users.noreply.github.com>
2022-10-06 16:57:10 +0530

    Add files via upload
    This is Image uploader project in django

---
+++ Image_uploader/imageuploader/myapp/forms.py
@@ -0,0 +1,12 @@
+from pyexpat import model
+from attr import fields
+from django import forms
+from .models import Image
+
+
+class ImageForm(forms.ModelForm):
+    class Meta:
+        model= Image
+        fields = '__all__'
+        labels ={'photo':''}
+        
